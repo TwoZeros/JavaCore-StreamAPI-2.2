1cc2fa6daf203e406ce65c9525a6305fca4d4ce2
Kirill <pak.kiruha@yandex.ru>
2022-05-15 00:57:58 +0300

    fix error

--- src/ru/twozeros/netology/stream/task2/Main.java
+++ src/ru/twozeros/netology/stream/task2/Main.java
@@ -16,20 +16,21 @@ public class Main {
                     Sex.values()[new Random().nextInt(Sex.values().length)],
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
-            //Получаем количество несовершеннолетних
-            Long countYoung = persons.stream().filter(x -> x.getAge() < 18).count();
-            System.out.println(countYoung);
-            //список фамилий призывников (т.е. мужчин от 18 и до 27 лет)
-            List<String> recruitList = persons.stream().filter(x -> x.getSex() == Sex.MAN)
-                    .filter(x -> x.getAge() >= 18 && x.getAge() < 27).map(Person::getFamily).toList();
-            //System.out.println(recruitList); Их слишком много чтоб выводить в консоль
-            //отсортированный по фамилии список потенциально работоспособных людей с высшим образованием в выборке
-            // (т.е. людей с высшим образованием от 18 до 60 лет для женщин и до 65 лет для мужчин)
-            List<Person> workerList = persons.stream().filter(x -> x.getEducation() == Education.HIGHER)
-                    .filter(person -> person.getSex() == Sex.MAN ? (person.getAge() >= 18 && person.getAge() < 65) :
-                            (person.getAge() >= 18 && person.getAge() < 60)).sorted(Comparator.comparing(Person::getFamily)).toList();
-            //System.out.println(workerList); Их слишком много чтоб выводить в консоль
         }
+        //Получаем количество несовершеннолетних
+        Long countYoung = persons.stream().filter(x -> x.getAge() < 18).count();
+        System.out.println(countYoung);
+        //список фамилий призывников (т.е. мужчин от 18 и до 27 лет)
+        List<String> recruitList = persons.stream().filter(x -> x.getSex() == Sex.MAN)
+                .filter(x -> x.getAge() >= 18 && x.getAge() < 27).map(Person::getFamily).toList();
+        //System.out.println(recruitList); Их слишком много
+        //отсортированный по фамилии список потенциально работоспособных людей с высшим образованием в выборке
+        // (т.е. людей с высшим образованием от 18 до 60 лет для женщин и до 65 лет для мужчин)
+        List<Person> workerList = persons.stream().filter(x -> x.getEducation() == Education.HIGHER)
+                .filter(person -> person.getSex() == Sex.MAN ? (person.getAge() >= 18 && person.getAge() < 65) :
+                        (person.getAge() >= 18 && person.getAge() < 60)).sorted(Comparator.comparing(Person::getFamily)).toList();
+        //System.out.println(workerList); Их слишком много
+
     }
 
 }
